5fb306e1b85bc8dafedbac2f2e7cbdf4cd67e8ac
st17064hh <st17064hh@gm.ibaraki-ct.ac.jp>
2022-10-07 16:35:46 +0900

    teacher修正後

--- frontend/src/pages/teacher/teacher.tsx
+++ frontend/src/pages/teacher/teacher.tsx
@@ -6,29 +6,29 @@ import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
+import { Button } from "@mui/material";
 
 function createData(
   subject: string,
   classes: string,
   time: string,
-  student: string,
 ) {
-  return { subject, classes, time, student };
+  return { subject, classes, time };
 }
 
 const rows = [
-  createData('科目A', '5I', 'n1曜日n1時間目', ' '),
-  createData('科目B', '5I', 'n2曜日n2時間目', ' '),
-  createData('科目C', '4I', 'n3曜日n3時間目', ' '),
-  createData('科目D', '3I', 'n4曜日n4時間目', ' '),
-  createData('科目E', '2I', 'n5曜日n5時間目', ' '),
+  createData('科目A', '5I', 'n1曜日n1時間目'),
+  createData('科目B', '5I', 'n2曜日n2時間目'),
+  createData('科目C', '4I', 'n3曜日n3時間目'),
+  createData('科目D', '3I', 'n4曜日n4時間目'),
+  createData('科目E', '2I', 'n5曜日n5時間目'),
 ];
 
 export const Teacher = () => {
 
     return(
       <div>
-        <h1 style={{color: "green"}}>担当科目</h1>
+        <h1 style={{color: "green"}}>担当科目(T沢先生)</h1>
         <TableContainer component={Paper}>
           <Table sx={{ minWidth: 650 }} aria-label="simple table">
             <TableHead>
@@ -36,7 +36,7 @@ export const Teacher = () => {
                 <TableCell>科目</TableCell>
                 <TableCell align="right">クラス</TableCell>
                 <TableCell align="right">時間</TableCell>
-                <TableCell align="right">履修している学生</TableCell>
+                <TableCell align="right"></TableCell>
               </TableRow>
             </TableHead>
             <TableBody>
@@ -50,7 +50,7 @@ export const Teacher = () => {
                   </TableCell>
                   <TableCell align="right">{row.classes}</TableCell>
                   <TableCell align="right">{row.time}</TableCell>
-                  <TableCell align="right">{row.student}</TableCell>
+                  <TableCell align="right"><Button variant="contained">履修している学生一覧</Button></TableCell>
                 </TableRow>
               ))}
             </TableBody>
