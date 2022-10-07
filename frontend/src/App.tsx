aacec64796b940c39a94a80f88b28910f5f48a49
st17064hh <st17064hh@gm.ibaraki-ct.ac.jp>
2022-10-07 17:03:17 +0900

    履修している学生の表

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,6 +3,7 @@ import { Route, BrowserRouter, Routes } from 'react-router-dom';
 import { Student } from './pages/student/student';
 import { Teacher } from './pages/teacher/teacher';
 import { Home } from './pages/home/home';
+import { RegisteredStudents } from './pages/registeredStudents/registeredStudents';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/student/*" element={<Student />} />;
         <Route path="/teacher/*" element={<Teacher />} />
+        <Route path="/registeredStudents/*" element={<RegisteredStudents />} />
       </Routes>
     </BrowserRouter>
   );
---
+++ frontend/src/pages/registeredStudents/registeredStudents.tsx
@@ -0,0 +1,53 @@
+import * as React from 'react';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
+import { Button } from "@mui/material";
+
+function createData(
+  name: string,
+) {
+  return { name };
+}
+
+const rows = [
+  createData('鈴木'),
+  createData('渡辺'),
+  createData('田中'),
+  createData('山田'),
+];
+
+export const RegisteredStudents = () => {
+
+    return(
+      <div>
+        <h1 style={{color: "green"}}>担当科目(T沢先生)</h1>
+        <TableContainer component={Paper}>
+          <Table sx={{ minWidth: 650 }} aria-label="simple table">
+            <TableHead>
+              <TableRow>
+                <TableCell>学生の名前</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {rows.map((row) => (
+                <TableRow
+                  key={row.name}
+                  sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                >
+                  <TableCell component="th" scope="row">
+                    {row.name}
+                  </TableCell>
+                  <TableCell align="right"><Button variant="contained">戻る</Button></TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </TableContainer>
+      </div>
+    );
+}
